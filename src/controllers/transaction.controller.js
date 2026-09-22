499aa57e5fbb7800f7382637d59f863a97881d25
parthsawang <parthsawang61@gmail.com>
2026-09-22 23:34:06 +0530

    validation request for transaction controller

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -1,5 +1,6 @@
 const transactionModel = require("../model/transation.model");
-
+const ledgerModel = require('../model/ledger.model')
+const accountModel = require('../model/ledger.model')
 async function createTransaction(req, res) {
     const {
         fromAccount,
@@ -8,9 +9,16 @@ async function createTransaction(req, res) {
         idempotencyKey
     } = req.body;
 
+
+    /**
+     * --Validates Request
+     */
+
     if (!fromAccount || !toAccount || !amount || !idempotencyKey) {
         return res.status(400).json({
             message: "All transaction fields are required"
         });
     }
 }
+
+
