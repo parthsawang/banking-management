4fbb49eacb1e5c2175e82a7d6e5afbc09b9e74e7
parthsawang <parthsawang61@gmail.com>
2026-09-22 23:44:30 +0530

    feat: add transaction request and account validation

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -1,6 +1,7 @@
 const transactionModel = require("../model/transation.model");
 const ledgerModel = require('../model/ledger.model')
 const accountModel = require('../model/ledger.model')
+
 async function createTransaction(req, res) {
     const {
         fromAccount,
@@ -9,16 +10,43 @@ async function createTransaction(req, res) {
         idempotencyKey
     } = req.body;
 
-
-    /**
-     * --Validates Request
-     */
+/**
+ * - Create a new transaction
+ * THE 10-STEP TRANSFER FLOW:
+     * 1. Validate request
+     * 2. Validate idempotency key
+     * 3. Check account status
+     * 4. Derive sender balance from ledger
+     * 5. Create transaction (PENDING)
+     * 6. Create DEBIT ledger entry
+     * 7. Create CREDIT ledger entry
+     * 8. Mark transaction COMPLETED
+     * 9. Commit MongoDB session
+     * 10. Send email notification
+ */
 
     if (!fromAccount || !toAccount || !amount || !idempotencyKey) {
         return res.status(400).json({
-            message: "All transaction fields are required"
+            message: "fromACcount , toAccount , amount and idempotencyKey is required "
         });
     }
+
+    const fromUserAccount = await accountModel.findOne({
+        _id: fromAccount,
+    })
+
+     const toUserAccount = await accountModel.findOne({
+        _id: toAccount,
+    })
+
+    if(!fromUserAccount || !toUserAccount){
+         return res.status(404).json({
+            message: "Account is not Found"
+        });
+    }
+
 }
+
+
 
 
